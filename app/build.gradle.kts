519dc3fb7d1374111a1fd2b312b61270ba05443e
AungSoe Moe <61192036+whispermmepub@users.noreply.github.com>
2026-09-02 10:10:25 +0700

    Bump WoW Note to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.whispermmepub.wowcolornote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes {
